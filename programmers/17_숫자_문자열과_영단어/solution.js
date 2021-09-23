b27da0097b9d22330cc244d8cb07bf96cc2154cc
wuixwui <seoweet21@gmail.com>
2021-09-23 19:37:03 +0900

    17. 수정

--- programmers/17_숫자_문자열과_영단어/solution.js
+++ programmers/17_숫자_문자열과_영단어/solution.js
@@ -1,11 +1,22 @@
 function solution(s) {
-  const englishToNumber = ["zero","one","two","three","four","five","six","seven","eight","nine"]
->
-  for (let english in englishToNumber){
-      while(s.indexOf(englishToNumber[english]) !== -1){
-          s = s.replace(englishToNumber[english], english)
-      }
+  const englishToNumber = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+
+  for (let english in englishToNumber) {
+    while (s.indexOf(englishToNumber[english]) !== -1) {
+      s = s.replace(englishToNumber[english], english);
+    }
   }
->
+
   return parseInt(s);
 }
